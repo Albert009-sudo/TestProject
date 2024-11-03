84c0ad329cfc1056b455756fe5ef80ccb001dbfa
Sandro Dolidze <dolidzesandro8@gmail.com>
2024-11-04 01:43:11 +0400

    docker naming

--- src/main/java/com/example/testproject/TestProjectApplication.java
+++ src/main/java/com/example/testproject/TestProjectApplication.java
@@ -2,12 +2,15 @@ package com.example.testproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 //Run project from here
 @EnableWebMvc
-@SpringBootApplication
+@SpringBootApplication(exclude = {JpaRepositoriesAutoConfiguration.class})
+@EnableJpaRepositories("com.example.testproject.Repository")
 @EntityScan("com.example.testproject.Entity")
 public class TestProjectApplication {
 
